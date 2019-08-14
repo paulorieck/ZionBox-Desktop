719c2fb62d93270e1c9d8e2b9bba78e4be395e90
paulorieck <paulo.rieck@prodgectool.com>
2019-08-14 16:17:16 -0300

    Creation of Desktop icon on Windows

--- postinstall.js
+++ postinstall.js
@@ -3,29 +3,46 @@
 const os = require('os');
 const path = require('path');
 
-if ( os.platform() === "win32" ) {
+const exec = require('child-process').exec;
 
-    const windows_shortcut_vbs = require("windows-shortcut-vbs");
+var plataform_dependent_modules_installation = exec("node node_modules/platform-dependent-modules/cli.js");
 
-    var origin_path = path.join(__dirname, "startup.vbs");
+plataform_dependent_modules_installation.stdout.on('data', (data) => {
+    console.log(`install_pm2 => stdout: ${data}`);
+});
 
-    //////////////////////
-    // Creates icon on user's Destkop
+plataform_dependent_modules_installation.stderr.on('data', (data) => {
+    console.log(`install_pm2 => stderr: ${data}`);
+});
 
-    windows_shortcut_vbs.createShortcutInSpecialFolder("Desktop", origin_path, "ZionBox").then( (shortcutPath) => {
-        console.log(`Shortcut path: ${shortcutPath}`);
-    }).catch( (err) => {
-        console.log(err);
-    });
+plataform_dependent_modules_installation.on('close', (code) => {
 
-    //////////////////////
-    // Creates icon on user's Menu
+    if ( os.platform() === "win32" ) {
 
-    //%AppData%\Microsoft\Windows\Start Menu\Programs
-    windows_shortcut_vbs.createShortcutInSpecialFolder("AppData\\Microsoft\\Windows\\Start Menu\\Programs", origin_path, "ZionBox").then( (shortcutPath) => {
-        console.log(`Shortcut path: ${shortcutPath}`);
-    }).catch( (err) => {
-        console.log(err);
-    });
+        const windows_shortcut_vbs = require("windows-shortcut-vbs");
+    
+        var origin_path = path.join(__dirname, "startup.vbs");
+    
+        //////////////////////
+        // Creates icon on user's Destkop
+    
+        windows_shortcut_vbs.createShortcutInSpecialFolder("Desktop", origin_path, "ZionBox").then( (shortcutPath) => {
+            console.log(`Shortcut path: ${shortcutPath}`);
+        }).catch( (err) => {
+            console.log(err);
+        });
+    
+        //////////////////////
+        // Creates icon on user's Menu
+    
+        //%AppData%\Microsoft\Windows\Start Menu\Programs
+        windows_shortcut_vbs.createShortcutInSpecialFolder("AppData\\Microsoft\\Windows\\Start Menu\\Programs", origin_path, "ZionBox").then( (shortcutPath) => {
+            console.log(`Shortcut path: ${shortcutPath}`);
+        }).catch( (err) => {
+            console.log(err);
+        });
+    
+    }
 
-}
+});
+
